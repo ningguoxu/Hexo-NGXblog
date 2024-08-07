64eefd00847b1b725cbc738a69ac376baf4ddce1
ningguoxu <ningguoxu123456@163.com>
2024-08-07 14:07:13 +0800

    Site updated: 2024-08-07 14:07:09

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/06/hello-world/"];function toRandomPost(){
+var posts=["2024/08/07/Hexo搭建安之鱼主题配置/","2024/08/06/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
